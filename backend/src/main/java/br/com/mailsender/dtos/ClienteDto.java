bba35356cb5580eeb6ee8216dc628bd9a7034ccf
gabrmo1 <gabrielmoreno1jlle@gmail.com>
2023-07-10 18:21:20 -0300

    Adicionando validações adicionais nos Dtos

--- backend/src/main/java/br/com/mailsender/dtos/ClienteDto.java
+++ backend/src/main/java/br/com/mailsender/dtos/ClienteDto.java
@@ -2,8 +2,10 @@ package br.com.mailsender.dtos;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.hateoas.RepresentationModel;
 
 @Data
@@ -13,12 +15,15 @@ public class ClienteDto extends RepresentationModel<ClienteDto> {
     private String chave;
 
     @NotBlank(message = "O nome precisa ser informado")
+    @Length(max = 100, message = "O nome pode ter no máximo 100 caracteres")
     private String nome;
 
     @NotBlank(message = "o email precisa ser informado")
     @Email(message = "O formato do e-mail está incorreto")
+    @Length(max = 150, message = "O email pode ter no máximo 150 caracteres")
     private String email;
 
+    @Pattern(regexp = "^\\d{4}-\\d{2}-\\d{2}$", message = "Formato de data inválido. Use o formato yyyy-MM-dd")
     @NotBlank(message = "A data de nascimento precisa ser informada")
     private String dataNascimento;
 
--- backend/src/main/java/br/com/mailsender/dtos/NoticiaDto.java
+++ backend/src/main/java/br/com/mailsender/dtos/NoticiaDto.java
@@ -3,6 +3,7 @@ package br.com.mailsender.dtos;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.hibernate.validator.constraints.Length;
 import org.springframework.hateoas.RepresentationModel;
 
 @Data
@@ -12,6 +13,7 @@ public class NoticiaDto extends RepresentationModel<NoticiaDto> {
     private String chave;
 
     @NotBlank(message = "O título precisa ser informado")
+    @Length(max = 50, message = "O título pode ter no máximo 50 caracteres")
     private String titulo;
 
     @NotBlank(message = "A descrição precisa ser informada")
