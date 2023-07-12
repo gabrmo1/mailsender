790aca008c75166e994e586da02b945fbe595ea3
gabrmo1 <gabrielmoreno1jlle@gmail.com>
2023-07-11 23:34:21 -0300

    Adicionando despedida no fim do corpo do e-mail

--- backend/src/main/java/br/com/mailsender/util/AgendadorEnvioEmailUtils.java
+++ backend/src/main/java/br/com/mailsender/util/AgendadorEnvioEmailUtils.java
@@ -83,6 +83,10 @@ public class AgendadorEnvioEmailUtils {
                     .append("</div>");
         }
 
+        corpoEmail.append("<br>");
+        corpoEmail.append("<div style=\"margin-left: 15px;\"> Até a próxima! </div>");
+        corpoEmail.append("<br>");
+
         return corpoEmail.toString();
     }
 
